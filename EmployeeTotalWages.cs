3b7595eeda5e2fe8b54faeac91e5e2753cd64994
jayukasode23 <jayukasode2398@gmail.com>
2022-10-14 15:48:54 +0530

    Added Employee Total Wages

---
+++ EmployeeTotalWages.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage5Program
+{
+    public class EmployeeTotalWages
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
+        public void CheckEmployeeMonth()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int TotalWorkingDays = 0;
+
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && TotalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                TotalWorkingDays++;
+
+                Random random = new Random();
+                int empCheck = random.Next(3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day : " + TotalWorkingDays + "EmpHrs" + empHrs);
+
+            }
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        }
+    }
+}
+    
